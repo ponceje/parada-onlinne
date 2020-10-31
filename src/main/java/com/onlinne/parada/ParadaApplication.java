7ee9457fb4dffda0461fc487083a6fc97b335529
PONCEJE <ponceje@oocl.com>
2020-10-31 11:42:37 +0800

    SETUP [Jeyem] Build: Set up

---
+++ src/main/java/com/onlinne/parada/ParadaApplication.java
@@ -0,0 +1,13 @@
+package com.onlinne.parada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlinne/parada/ParadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinne.parada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
